77dd590406eb5874479c6551ed0aa2bf2c4e11b6
AnandRoy8121 <anandroy151997@gmail.com>
2024-01-19 20:25:06 +0530

    integrated firebase and form validation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import {signOut} from 'firebase/auth'
+import {auth} from '../utils/firebase'
+import { useDispatch, useSelector } from 'react-redux'
+import {removeUser} from '../utils/userSlice'
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const selector = useSelector(store=>store.user)
+
+  const handleSignOut = async ()=>{
+    
+      const signOutUser = await signOut(auth);
+      dispatch(removeUser)
+      navigate('/')
+  }
   return (
-    <div className='flex items-center px-8 py-2 absolute bg-gradient-to-b from-black'>
-      <img className='w-full h-20' src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo" />
+    <div className='flex items-center px-6 py-1 w-full absolute bg-gradient-to-b from-black justify-between'>
+      <img className='w-32 h-20' src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo" />
+      <div>
+        {selector && <button className='bg-red-500 text-white font-semibold px-3 py-2 uppercase rounded-md' onClick={handleSignOut}>LogOut</button>}
+      </div>
     </div>
   )
 }
--- src/components/SignSignUpForm.jsx
+++ src/components/SignSignUpForm.jsx
@@ -1,38 +1,124 @@
 import React, { useState } from "react";
+import { validateForm } from "../utils/validate";
+import { auth } from "../utils/firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { addUser } from "../utils/userSlice";
+import { useDispatch } from "react-redux";
 
 const SignSignUpForm = () => {
-    const [isSignIn, setIsSignIn] = useState(true);
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [validationMessage, setValdationMessage] = useState(null);
+  const [isSignIn, setIsSignIn] = useState(true);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // handle form validation
+    const message = validateForm(email, password);
+    setValdationMessage(message);
+
+    if (validationMessage) return;
+
+    // if validation is successful then signIn/signUp
+    if (!isSignIn) {
+      // Sign up logic here
+      try {
+        const authenticate = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const user = authenticate.user;
+        updateProfile(user, {
+          displayName: name,
+        }).then(()=>{
+          const { uid, email, displayName } = user;
+          dispatch(addUser({ uid, email, displayName }));
+        });
+        
+        setEmail("");
+        setPassword("");
+        setName("");
+        navigate("/browse");
+      } catch (error) {
+        console.log(error.message);
+        setValdationMessage(error.code + " : " + error.message);
+      }
+    } else {
+      // Sign Logic here
+      try {
+        const authenticate = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        const user = authenticate.user;
+
+        // console.log(user)
+        setEmail("");
+        setPassword("");
+        navigate("/browse");
+      } catch (error) {
+        console.log(error.message);
+        setValdationMessage(error.code + " : " + error.message);
+      }
+    }
+  };
+
   return (
-    <form className="flex flex-col my-16 sm:my-28 mx-auto w-[80%] sm:h-[80%] sm:w-[35%] p-12 bg-black/60 absolute inset-0 rounded-md gap-6">
-        <h1 className="text-3xl font-semibold text-white">
-          {isSignIn ? "Sign In" : "Sign Up"}
-        </h1>
-        <div className="flex flex-col gap-6">
-          {!isSignIn && (
-            <input
-              className="bg-[#333] p-4 w-full text-gray-300 rounded-md"
-              type="text"
-              placeholder="Name"
-              name="Name"
-            />
-          )}
+    <form
+      className="flex flex-col mt-28 mx-auto w-[80%] sm:w-[35%] p-12 bg-black/60 absolute inset-0 rounded-md gap-6"
+      onSubmit={handleSubmit}
+    >
+      <h1 className="text-3xl font-semibold text-white">
+        {isSignIn ? "Sign In" : "Sign Up"}
+      </h1>
+      <p className="text-red-400 text-sm p-1 -my-4">{validationMessage}</p>
+      <div className="flex flex-col gap-6">
+        {!isSignIn && (
           <input
             className="bg-[#333] p-4 w-full text-gray-300 rounded-md"
             type="text"
-            placeholder="Email or phone number"
-            name="email"
+            required
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            name="name"
           />
-          <input
-            className="bg-[#333] p-4 w-full text-gray-300 rounded-md"
-            type="password"
-            placeholder="Password"
-            name="email"
-          />
-        </div>
-        <button className="bg-[#e50914] font-semibold p-4 w-full text-white rounded-md">
-          {isSignIn?"Sign In":"Sign Up"}
-        </button>
-        {isSignIn &&<div className="flex items-center -mt-5 text-[#333] justify-between text-sm sm:flex-row flex-col">
+        )}
+        <input
+          className="bg-[#333] p-4 w-full text-gray-300 rounded-md"
+          type="text"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email or phone number"
+          name="email"
+        />
+        <input
+          className="bg-[#333] p-4 w-full text-gray-300 rounded-md"
+          type="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          name="email"
+        />
+      </div>
+      <button className="bg-[#e50914] font-semibold p-4 w-full text-white rounded-md">
+        {isSignIn ? "Sign In" : "Sign Up"}
+      </button>
+      {isSignIn && (
+        <div className="flex items-center -mt-5 text-[#333] justify-between text-sm sm:flex-row flex-col">
           <div className="flex items-center gap-1">
             <input
               className="bg-gray-500"
@@ -43,29 +129,30 @@ const SignSignUpForm = () => {
             <span className="text-gray-300">Remember Me</span>
           </div>
           <span className="text-gray-300">Need Help?</span>
-        </div>}
-        {isSignIn ?
-          <div className="flex gap-1 items-center flex-col sm:flex-row">
-            <span className="text-gray-500">New to Netflix?</span>
-            <span
-              className="text-white hover:underline cursor-pointer"
-              onClick={() => setIsSignIn(false)}
-            >
-              Sign up now
-            </span>
-          </div>
-         : 
-          <div className="flex gap-1 items-center flex-col sm:flex-row">
-            <span className="text-gray-500">Already a Netflix User?</span>
-            <span
-              className="text-white hover:underline cursor-pointer"
-              onClick={() => setIsSignIn(!isSignIn)}
-            >
-              Sign in
-            </span>
-          </div>
-        }
-      </form>
+        </div>
+      )}
+      {isSignIn ? (
+        <div className="flex gap-1 items-center flex-col sm:flex-row">
+          <span className="text-gray-500">New to Netflix?</span>
+          <span
+            className="text-white hover:underline cursor-pointer"
+            onClick={() => setIsSignIn(false)}
+          >
+            Sign up now
+          </span>
+        </div>
+      ) : (
+        <div className="flex gap-1 items-center flex-col sm:flex-row">
+          <span className="text-gray-500">Already a Netflix User?</span>
+          <span
+            className="text-white hover:underline cursor-pointer"
+            onClick={() => setIsSignIn(!isSignIn)}
+          >
+            Sign in
+          </span>
+        </div>
+      )}
+    </form>
   );
 };
 
