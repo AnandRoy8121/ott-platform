99a24a40d8d5035bb77b115b02aabdecaa8e9ccb
AnandRoy8121 <anandroy151997@gmail.com>
2024-01-21 10:49:34 +0530

    Created user Icon component on the header and added logout functionality inside dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,45 +1,37 @@
-import React, { useEffect } from 'react'
-import {onAuthStateChanged, signOut} from 'firebase/auth'
-import {auth} from '../utils/firebase'
-import { useDispatch, useSelector } from 'react-redux'
-import {addUser, removeUser} from '../utils/userSlice'
-import { useNavigate } from 'react-router-dom'
-import { LOGO } from '../utils/constants'
+import React, { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { LOGO } from "../utils/constants";
+import UserIcon from "./UserIcon";
 
 const Header = () => {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const selector = useSelector(store=>store.user)
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const selector = useSelector((store) => store.user);
 
-  useEffect(()=>{
-    const unsubscribe = onAuthStateChanged(auth,(user)=>{
-
-      if(user){
-        const {uid,email,displayName} = user
-        dispatch(addUser({uid,email,displayName}))
-        navigate('/browse')
-      }else{
-        dispatch(removeUser())
-        navigate('/')
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName } = user;
+        dispatch(addUser({ uid, email, displayName }));
+        navigate("/browse");
+      } else {
+        dispatch(removeUser());
+        navigate("/");
       }
-    })
-    return ()=> unsubscribe()
-  },[])
+    });
+    return () => unsubscribe();
+  }, []);
 
-  const handleSignOut = async ()=>{
-    
-      const signOutUser = await signOut(auth);
-      dispatch(removeUser)
-      navigate('/')
-  }
   return (
-    <div className='flex items-center px-6 py-1 w-full absolute bg-gradient-to-b from-black justify-between z-50'>
-      <img className='w-24 h-20 md:w-32 md:h-20' src={LOGO} alt="logo" />
-      <div>
-        {selector && <button className='bg-red-500 text-white font-semibold px-3 py-2 uppercase rounded-md' onClick={handleSignOut}>LogOut</button>}
-      </div>
+    <div className="flex items-center px-6 py-1 w-full absolute bg-gradient-to-b from-black justify-between z-50">
+      <img className="w-24 h-20 md:w-32 md:h-20" src={LOGO} alt="logo" />
+      {selector && <UserIcon />}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux'
 
 const SecondaryContainer = () => {
   const movies = useSelector(store=>store.movies)
-  return (movies &&<div className='flex flex-col bg-black'>
+  return (movies &&<div className='flex flex-col bg-black pb-6'>
       <div className=' relative rounded-lg px-8 text-white sm:-mt-16 md:-mt-28'>
       <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies}/>
       <MovieList title={"Popular Movies"} movies={movies?.popularMovies}/>
